89bb3b6319e0ac7484c06facd0edc85583d086ab
Rohitsiswal88 <40537213+Rohitsiswal88@users.noreply.github.com>
2018-07-04 00:28:19 +0530

    Intro to Python

---
+++ Assign1.py
@@ -0,0 +1,23 @@
+1) Using print
+print(“Hello World”)
+
+2) joining two strings
+      print(“Acad”+”View”)
+
+3) Using input statement
+     x=input(“Enter the value of x”)
+     y=input(“Enter the value of y”)
+     z=input(“Enter the value of z”)
+     print(“The value of x is”,x)
+     print(“The value of y is”,y)
+     print(“The value of z is”,z)
+
+4) using print statement
+      print(“Let’s get started”)
+
+ 5)  s="Acadview"
+       course="Python"
+       fees = 5000
+       print(('%s''%s''%d')%(s,course,fees))
+
+6) (_name___,_salary__)
